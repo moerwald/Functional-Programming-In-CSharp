3c6d92f85bda0e5b7bee7895b5a7ac77dc443150
moerwald <andreas.mewald@gmail.com>
2023-01-06 21:02:19 +0100

    CH8 added example

---
+++ src/CH8/PizzaOrder/PizzaOrder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
+using LaYumba;
+using LaYumba.Functional;
+
+using Unit = System.ValueTuple;
+
+public static class Programm
+{
+    private static Unit TheUnit = new Unit();
+
+    public static void Main(string[] _)
+    {
+        Func<Either<Reason, Unit>> WakeUpEarly = () => TheUnit;
+        Func<Either<Reason, Ingredients>> ShopForIngredients = () => new Ingredients( ImmutableList.Create<string>("I1", "I2"));
+        Func<Ingredients, Either<Reason, Food>> CookRecipe = i => new Food("Hot dog");
+
+        Action<Food> EnjoyToGether = f => Console.WriteLine($"Yummy {f}");
+        Action<Reason> ComplainAbout = r => Console.WriteLine($"Not ok: {r}");
+        Action OrderPizza = () => Console.WriteLine("We're ordering pizza");
+
+        WakeUpEarly()
+            .Bind(_ => ShopForIngredients())
+            .Bind(CookRecipe)
+            .Match(reason =>
+            {
+                ComplainAbout(reason);
+                OrderPizza();
+            }, EnjoyToGether);
+    }
+}
+
+public record Food(string Name);
+public record Ingredients(ImmutableList<string> Ingreds);
+public record Reason (string Description);
